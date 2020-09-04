b250d635eb93b0881054eb2936ba6fc249826116
Riya Panchal <riyap1231@gmail.com>
2020-09-04 11:38:36 +0530

    first commit

---
+++ SpeakHello.js
@@ -0,0 +1,15 @@
+(function (window) {
+    
+    // STEP 3
+    var helloSpeaker = {};
+    
+    var speakWord = "Hello";
+    
+    // STEP 4
+    helloSpeaker.speak = function (name) {
+      console.log(speakWord + " " + name);
+    }
+    // STEP 5
+    window.helloSpeaker = helloSpeaker;
+    
+    })(window);
